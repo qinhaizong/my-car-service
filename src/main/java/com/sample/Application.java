8fe56b18b1b789f673d10b4b86a160c3bdca9588
qinhaizong <262878749@qq.com>
2018-06-27 23:38:34 +0800

    remove cxf

--- src/main/java/com/sample/Application.java
+++ src/main/java/com/sample/Application.java
@@ -1,12 +1,16 @@
 package com.sample;
 
-import org.apache.cxf.jaxrs.servlet.CXFNonSpringJaxrsServlet;
-import org.apache.olingo.odata2.core.rest.app.ODataApplication;
+import org.apache.olingo.odata2.core.servlet.ODataServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
+import static org.apache.olingo.odata2.api.ODataServiceFactory.FACTORY_LABEL;
+
+/**
+ * @author qinhaizong
+ */
 @SpringBootApplication
 public class Application {
 
@@ -15,11 +19,9 @@ public class Application {
     }
 
     @Bean
-    public ServletRegistrationBean cxfNonSpringJaxrsServletRegistrationBean() {
-        ServletRegistrationBean bean = new ServletRegistrationBean(new CXFNonSpringJaxrsServlet(), "/CarService.svc/*");
-        bean.addInitParameter("javax.ws.rs.Application", ODataApplication.class.getName());
-        bean.addInitParameter("org.apache.olingo.odata2.service.factory", CarServiceFactory.class.getName());
-        bean.setLoadOnStartup(1);
+    public ServletRegistrationBean odataServletRegistrationBean() {
+        ServletRegistrationBean bean = new ServletRegistrationBean(new ODataServlet(), "/CarService.svc/*");
+        bean.addInitParameter(FACTORY_LABEL, CarServiceFactory.class.getName());
         return bean;
     }
 }
